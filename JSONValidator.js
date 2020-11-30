4cb8803de49e005b6c44e35e64ec51ec4fdf45ca
Mattias Ekdahl <matekd@student.chalmers.se>
2020-11-30 19:37:34 +0100

    Add publish and subscribe functionality using mqtt

--- JSONValidator.js
+++ JSONValidator.js
@@ -1,7 +1,18 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://localhost:1883')
 // Matches the time attribute from a booking request
 // E.g. "YYYY-(MM or M)-(DD or D) (HH or H):MM" but with numbers "1234-12-1 14:30"
 // ^ and $ to check that the entire string matches, only contains what we expect
 const timeRegex = /^\d\d\d\d-(\d\d|\d)-(\d\d|\d)\s(\d\d|\d):\d\d$/
+
+client.on('connect', function () {
+    client.subscribe('bookingRequest')
+})
+
+client.on('message', function (topic, message) {
+  message = message.toString()
+  IsJsonString(message)
+})
 
 function IsJsonString(str) {
   try {
@@ -17,10 +28,18 @@ function IsJsonString(str) {
         typeof json.time === "string") {
         // Check that time matches timeRegex
         if(timeRegex.test(json.time)){
-          return true
+          // Creates a new json object with the valid attributes, discards any extra attributes
+          var validReq = {
+            "userid": json.userid,
+            "requestid": json.requestid,
+            "dentistid": json.dentistid,
+            "issuance": json.issuance,
+            "time": json.time
+          }
+          // Converts object to a string and publishes
+          client.publish('validBookingRequest', JSON.stringify(validReq))
         }
       }
     }
   } catch (e) {}
-  return false
 }
